30fe42ef2383baf857b39e1b7730c9c4fc47a194
RITESH THAKUR <thakurram2668@gmail.com>
2022-08-11 01:30:44 +0530

    initial commit

---
+++ client/src/components/Nav.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = ({socket}) => {
+  const [notification, setNotification] = useState("")
+
+  useEffect(()=> {
+    socket.on("addProductResponse", data => {
+      setNotification(`@${data.owner} just added ${data.name} worth $${Number(data.price).toLocaleString()}`)
+    })
+  }, [socket])
+  
+  useEffect(()=> {
+    socket.on("bidProductResponse", data => {
+      setNotification(`@${data.last_bidder} just bid ${data.name} for $${Number(data.amount).toLocaleString()}`)
+    })
+  }, [socket])
+
+  return (
+    <nav className='navbar'>
+      <div className='header'>
+        <h2 ><Link className='home-btn' to="/" >Bidding System</Link></h2>
+      </div>
+            
+      <div>
+        <p style={{color: "red"}}>{notification}</p>
+      </div>       
+    </nav>
+  )
+}
+
+export default Nav
